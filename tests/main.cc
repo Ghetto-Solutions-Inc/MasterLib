eb265a0e46d97a003e185f1722281692434b81ec
Razzile <comfycallum@gmail.com>
2017-08-27 01:00:38 +0100

    Add assembly tests

--- tests/main.cc
+++ tests/main.cc
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <iomanip>
 #include <masterlib/process.h>
+#include <masterlib/disassembler.h>
+#include <masterlib/assembler.h>
 #include <mach-o/dyld.h>
-using masterlib::Process;
+
+
+using namespace masterlib;
 
 inline void print_region(const Process::Region &region) {
     std::cout << std::hex << region.start() << " "
               << std::hex << region.size() << " "
               << std::hex << region.prot() << std::endl;
+}
+
+inline void print_instr(const InstrRef &instr) {
+    std::cout << instr->mnemonic()
+              << " "
+              << instr->operands()
+              << " ";
+    for (auto &b: instr->bytes()) {
+        std::cout << std::setfill('0') << std::setw(2) << std::hex << (int)b;
+    }
+    std::cout << std::endl;
 }
 
 int main() {
@@ -23,4 +39,26 @@ int main() {
         print_region(region);
     }
 
+    const uint8_t code[] = {
+            0x00, 0x00, 0xA0, 0xE1,
+            0x00, 0xFF, 0xA0, 0xE3,
+            0x1E, 0xFF, 0x2F, 0xE1
+    };
+    Disassembler ds(0x0, code, sizeof(code), ArchConfig::ARMConfig);
+    ds.Setup();
+
+    InstrRef instr = ds.Disassemble();
+
+    //print_instr(instr);
+
+    ds.DisassembleIter([](InstrRef &instr) {
+        //print_instr(instr);
+        return true;
+    });
+
+    Assembler as("MOV R0, R1", 0x0, ArchConfig::ARMConfig);
+    as.Setup();
+
+    InstrRef assembled = as.Assemble();
+    print_instr(assembled);
 }
